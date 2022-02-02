19c1ccc2ccc9a8edd8d650b2495c3f39f5ce138d
Mosiur12 <mrsojib9600@gmail.com>
2022-02-03 02:23:40 +0600

    solving

---
+++ problem.js
@@ -0,0 +1,15 @@
+const getString = 'Hello brother! how are you?';
+function PutString (text){
+let reverse = '';
+    for(const letter of text){
+       
+        reverse =  letter + reverse;
+
+    }
+return reverse;
+}
+const reversed = PutString (getString);
+console.log(reversed);
+
+
+
