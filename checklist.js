6b9e136c57bca4a4ac16760f798edd28855f4978
Mosiur12 <mrsojib9600@gmail.com>
2022-02-04 00:50:36 +0600

    another-problem-js

---
+++ checklist.js
@@ -0,0 +1,36 @@
+// var num1 = 15;
+// var num2 = 20;
+// let result = num1 + num2;
+// console.log(result);
+
+// var num = 1;
+// while(num <19){
+//     console.log(num);
+//     num = num+2;
+    
+// }
+// var nameTo = [12, 25, 36, 29, 65];
+
+// var position = nameTo.indexOf(65);
+
+// console.log(position);
+
+
+// nameTo[3] = 5;
+// console.log(nameTo);
+// nameTo.push(22);
+
+
+// nameTo.pop(65);
+// console.log(nameTo);
+
+// let arrName = ['bang', 'ind', 'can', 'ameri'];
+// for (let i=0; i< arrName.length; i++){
+//     console.log(arrName[i]);
+// }
+
+var pointNumber = [ 85, 36, 69, 95, 12, 89, 100, 25];
+let i=0;
+    console.log(pointNumber[i]>80);
+// 
+
--- problem.js
+++ problem.js
@@ -1,15 +1,16 @@
-const getString = 'Hello brother! how are you?';
-function PutString (text){
-let reverse = '';
-    for(const letter of text){
+// const getString = 'Hello brother! how are you?';
+// function PutString (text){
+// let reverse = '';
+//     for(const letter of text){
        
-        reverse =  letter + reverse;
+//         reverse =  letter + reverse;
 
-    }
-return reverse;
-}
-const reversed = PutString (getString);
-console.log(reversed);
+//     }
+// return reverse;
+// }
+// const reversed = PutString (getString);
+// console.log(reversed);
+
 
 
 
